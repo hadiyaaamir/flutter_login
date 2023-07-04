37282c6de83553d2703d5c3aed19041c2cef7208
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-07-04 13:11:57 +0500

    username and passowrd models

--- lib/login/model/password.dart
+++ lib/login/model/password.dart
@@ -1,1 +1,14 @@
 part of 'model.dart';
+
+enum PasswordValidationError { empty }
+
+class Password extends FormzInput<String, PasswordValidationError> {
+  const Password.pure() : super.pure('');
+  const Password.dirty([super.value = '']) : super.dirty();
+
+  @override
+  PasswordValidationError? validator(String value) {
+    if (value.isEmpty) return PasswordValidationError.empty;
+    return null;
+  }
+}
