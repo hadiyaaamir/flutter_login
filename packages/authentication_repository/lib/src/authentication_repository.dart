5f8be4f9199ebdc40e01509ac1e338b04c227510
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-07-05 11:44:21 +0500

    separate file for hardcoded auth

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+part 'authentication_hardcoded.dart';
+
 enum AuthenticationStatus { unknown, authenticated, unauthenticated }
 
 abstract class AuthenticationRepository {
@@ -7,30 +9,4 @@ abstract class AuthenticationRepository {
   Future<void> logIn({required String username, required String password});
   void logOut();
   void dispose();
-}
-
-class AuthenticationRepositoryHardcoded implements AuthenticationRepository {
-  final _controller = StreamController<AuthenticationStatus>();
-
-  Stream<AuthenticationStatus> get status async* {
-    await Future<void>.delayed(const Duration(seconds: 1));
-    yield AuthenticationStatus.unauthenticated;
-    yield* _controller.stream;
-  }
-
-  Future<void> logIn({
-    required String username,
-    required String password,
-  }) async {
-    await Future.delayed(
-      const Duration(milliseconds: 300),
-      () => _controller.add(AuthenticationStatus.authenticated),
-    );
-  }
-
-  void logOut() {
-    _controller.add(AuthenticationStatus.unauthenticated);
-  }
-
-  void dispose() => _controller.close();
 }
