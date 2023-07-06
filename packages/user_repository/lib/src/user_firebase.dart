fbb237266e74fd2742f59f56000482e9f8b803cd
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-07-06 11:29:38 +0500

    get user from collection (temp)

--- packages/user_repository/lib/src/user_firebase.dart
+++ packages/user_repository/lib/src/user_firebase.dart
@@ -10,32 +10,20 @@ class UserRepositoryFirebase extends UserRepository {
   Future<User?> getUser() async {
     if (_user != null) return _user;
 
-    print('getting user');
-    return Future.delayed(
-      const Duration(milliseconds: 300),
-      () => _user = User(
-        id: const Uuid().v4(),
-        firstName: 'Hardcoded',
-        lastName: 'Firebase',
-        email: 'h.aamir@gmail.com',
-        designation: 'Intern',
-      ),
-    );
-    //   final String? email = _firebaseAuth.currentUser?.email;
+    final String? email = _firebaseAuth.currentUser?.email;
 
-    //   if (email != null) {
-    //     await usersCollection
-    //         .where('email', isEqualTo: email)
-    //         .get()
-    //         .then((snapshot) {
-    //       if (snapshot.docs.isNotEmpty) {
-    //         _user = User.fromJson(snapshot.docs.first.data());
-    //       }
-    //     }).then((_) {
-    //       return _user;
-    //     });
-    //   }
+    if (email != null) {
+      await usersCollection
+          .where('email', isEqualTo: email)
+          .get()
+          .then((snapshot) {
+        if (snapshot.docs.isNotEmpty) {
+          _user = User.fromJson(snapshot.docs.first.data());
+        }
+      });
+      return _user;
+    }
 
-    //   return _user = null;
+    return _user = null;
   }
 }
