c27e0df9a04f99bb4bb18d3cf79682ba47f9cc0e
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-07-07 11:42:32 +0500

    profile event and state

--- lib/profile/bloc/profile_bloc.dart
+++ lib/profile/bloc/profile_bloc.dart
@@ -1,11 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_login/profile/model/model.dart';
+import 'package:formz/formz.dart';
 
 part 'profile_event.dart';
 part 'profile_state.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
-  ProfileBloc() : super(ProfileInitial()) {
+  ProfileBloc() : super(const ProfileState()) {
     on<ProfileEvent>((event, emit) {
       // TODO: implement event handler
     });
