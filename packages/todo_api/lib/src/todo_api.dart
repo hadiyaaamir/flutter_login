b2fc8c5c757ad078e3241fd56148bf6b1118deb5
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-07-10 16:55:12 +0500

    todo models and api

---
+++ packages/todo_api/lib/src/todo_api.dart
@@ -0,0 +1,31 @@
+import 'model/model.dart';
+
+abstract class TodosApi {
+  const TodosApi();
+
+  Stream<List<Todo>> getTodos();
+
+  /// Saves a [todo].
+  ///
+  /// If a [todo] with the same id already exists, it will be replaced.
+  Future<void> saveTodo(Todo todo);
+
+  /// Deletes the `todo` with the given id.
+  ///
+  /// If no `todo` with the given id exists, a [TodoNotFoundException] error is
+  /// thrown.
+  Future<void> deleteTodo(String id);
+
+  /// Deletes all completed todos.
+  ///
+  /// Returns the number of deleted todos.
+  Future<int> clearCompleted();
+
+  /// Sets the `isCompleted` state of all todos to the given value.
+  ///
+  /// Returns the number of updated todos.
+  Future<int> completeAll({required bool isCompleted});
+}
+
+/// Error thrown when a [Todo] with a given id is not found.
+class TodoNotFoundException implements Exception {}
