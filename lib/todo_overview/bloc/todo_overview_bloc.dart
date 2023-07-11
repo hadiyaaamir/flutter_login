604f9b3649d94bc2197108dbdb38293a9806d3f3
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-07-11 12:13:34 +0500

    todo overview state

--- lib/todo_overview/bloc/todo_overview_bloc.dart
+++ lib/todo_overview/bloc/todo_overview_bloc.dart
@@ -1,11 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_repository/todo_repository.dart';
 
 part 'todo_overview_event.dart';
 part 'todo_overview_state.dart';
 
 class TodoOverviewBloc extends Bloc<TodoOverviewEvent, TodoOverviewState> {
-  TodoOverviewBloc() : super(TodoOverviewInitial()) {
+  TodoOverviewBloc() : super(const TodoOverviewState()) {
     on<TodoOverviewEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/todo_overview/bloc/todo_overview_state.dart
+++ lib/todo_overview/bloc/todo_overview_state.dart
@@ -1,10 +1,41 @@
 part of 'todo_overview_bloc.dart';
 
-abstract class TodoOverviewState extends Equatable {
-  const TodoOverviewState();
-  
-  @override
-  List<Object> get props => [];
-}
+enum TodosOverviewStatus { initial, loading, success, failure }
 
-class TodoOverviewInitial extends TodoOverviewState {}
+final class TodoOverviewState extends Equatable {
+  const TodoOverviewState({
+    this.status = TodosOverviewStatus.initial,
+    this.todos = const [],
+    // this.filter = TodosViewFilter.all,
+    this.lastDeletedTodo,
+  });
+
+  final TodosOverviewStatus status;
+  final List<Todo> todos;
+  //TODO: add filter
+  // final TodosViewFilter filter;
+  final Todo? lastDeletedTodo;
+
+  TodoOverviewState copyWith({
+    TodosOverviewStatus Function()? status,
+    List<Todo> Function()? todos,
+    // TodosViewFilter Function()? filter,
+    Todo? Function()? lastDeletedTodo,
+  }) {
+    return TodoOverviewState(
+      status: status != null ? status() : this.status,
+      todos: todos != null ? todos() : this.todos,
+      // filter: filter != null ? filter() : this.filter,
+      lastDeletedTodo:
+          lastDeletedTodo != null ? lastDeletedTodo() : this.lastDeletedTodo,
+    );
+  }
+
+  // Iterable<Todo> get filteredTodos => filter.applyAll(todos);
+
+  @override
+  List<Object?> get props => [
+        status, todos, lastDeletedTodo,
+        //filter
+      ];
+}
